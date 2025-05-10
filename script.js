1e3b1a25d211ea876b16f7b2459b4d80bf3ed3c7
LeonAlbers <leonalbers5@gmail.com>
2025-05-10 17:27:47 +0200

    Refactor code structure for improved readability and maintainability

--- script.js
+++ script.js
@@ -35,35 +35,3 @@ navLinks.forEach((link) => {
     burgerIcon.src = "res/burger-icon.svg"; // Switch back to burger icon
   });
 });
-
-// Automatically show which section is active
-const sections = document.querySelectorAll("section");
-const navItems = document.querySelectorAll(".nav-item");
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    // Sichtbare Einträge filtern
-    const visibleEntries = entries.filter((entry) => entry.isIntersecting);
-
-    if (visibleEntries.length > 0) {
-      // Die sichtbarste Sektion finden
-      const mostVisible = visibleEntries.reduce((prev, current) =>
-        prev.intersectionRatio > current.intersectionRatio ? prev : current
-      );
-
-      const id = mostVisible.target.getAttribute("id");
-      const activeLink = document.querySelector(`.nav-item[href="#${id}"]`);
-
-      // Aktive Klasse aktualisieren
-      navItems.forEach((item) => item.classList.remove("active"));
-      if (activeLink) {
-        activeLink.classList.add("active");
-      }
-    }
-  },
-  {
-    threshold: 0.6, // 50% der Sektion sichtbar
-  }
-);
-
-sections.forEach((section) => observer.observe(section));
